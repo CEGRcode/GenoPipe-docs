4f71e54234c05574b3535a86cfa57a6d268ae430
Olivia Wen-Mei Lang <oliviawlang@gmail.com>
2023-03-08 17:05:53 -0500

    add pughlab logo to footer
    - add pughlab logo to the footer
    - switch docusaurus.config.js formatting from double quotes (`"`) to single quotes (`'`) (mimic latest docusaurus release)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,51 +1,51 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "GenoPipe",
-  tagline: "Run a background check on your NGS data",
-  url: "https://CEGRcode.github.io",
-  baseUrl: "/GenoPipe-docs/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/genopipe.ico",
+  title: 'GenoPipe',
+  tagline: 'Run a background check on your NGS data',
+  url: 'https://CEGRcode.github.io',
+  baseUrl: '/GenoPipe-docs/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/genopipe.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "CEGRcode", // Usually your GitHub org/user name.
-  projectName: "GenoPipe-docs", // Usually your repo name.
+  organizationName: 'CEGRcode', // Usually your GitHub org/user name.
+  projectName: 'GenoPipe-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/CEGRcode/GenoPipe-docs",
+          // Remove this to remove the 'edit this page' links.
+          editUrl: 'https://github.com/CEGRcode/GenoPipe-docs',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/CEGRcode/GenoPipe-docs",
-        },
+//        blog: {
+//          showReadingTime: true,
+//          // Please change this to your repo.
+//          // Remove this to remove the 'edit this page' links.
+//          editUrl: 'https://github.com/CEGRcode/GenoPipe-docs',
+//        },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -55,95 +55,98 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "GenoPipe",
+        title: 'GenoPipe',
         logo: {
-          alt: "GenoPipe Logo",
-          src: "img/genopipe.png",
+          alt: 'GenoPipe Logo',
+          src: 'img/genopipe.png',
         },
         items: [
           {
-            type: "doc",
-            docId: "welcome",
-            position: "left",
-            label: "General",
+            type: 'doc',
+            docId: 'welcome',
+            position: 'left',
+            label: 'General',
           },
           {
-            type: "doc",
-            docId: "epitopeid",
-            position: "left",
-            label: "EpitopeID",
+            type: 'doc',
+            docId: 'epitopeid',
+            position: 'left',
+            label: 'EpitopeID',
           },
           {
-            type: "doc",
-            docId: "deletionid",
-            position: "left",
-            label: "DeletionID",
+            type: 'doc',
+            docId: 'deletionid',
+            position: 'left',
+            label: 'DeletionID',
           },
           {
-            type: "doc",
-            docId: "strainid",
-            position: "left",
-            label: "StrainID",
+            type: 'doc',
+            docId: 'strainid',
+            position: 'left',
+            label: 'StrainID',
           },
           {
-            href: "https://github.com/CEGRcode/GenoPipe",
-            /*label: "GitHub",*/
-            position: "right",
+            href: 'https://github.com/CEGRcode/GenoPipe',
             className: "header-github-link",
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
+        logo: {
+          alt: 'Pugh Lab Logo',
+          src: 'img/pughlab_logo.svg',
+        },
         links: [
           {
-            title: "Module Docs",
+            title: 'Modules',
             items: [
               {
-                label: "EpitopeID",
-                to: "/docs/epitopeid",
+                label: 'EpitopeID',
+                to: '/docs/epitopeid',
               },
               {
-                label: "DeletionID",
-                to: "/docs/deletionid",
+                label: 'DeletionID',
+                to: '/docs/deletionid',
               },
               {
-                label: "StrainID",
-                to: "/docs/strainid",
+                label: 'StrainID',
+                to: '/docs/strainid',
               },
             ],
           },
           {
-            title: "Community",
+            title: 'Community',
             items: [
               {
-                label: "Pugh Lab Website",
-                href: "https://pughlab.mbg.cornell.edu/",
+                label: 'Pugh Lab',
+                href: 'https://pughlab.mbg.cornell.edu',
               },
               {
-                label: "Lai Lab",
-                href: "https://williamkmlai.github.io",
+                label: 'Lai Lab',
+                href: 'https://williamkmlai.github.io',
               },
             ],
           },
           {
-            title: "Other Tools We Develop",
+            title: 'Other Tools We Develop',
             items: [
               {
-                label: "ScriptManager",
-                href: "https://github.com/CEGRcode/scriptmanager-docs",
+                label: 'ScriptManager',
+                href: 'http://pughlab.mbg.cornell.edu/scriptmanager-docs',
               },
               {
-                label: "PEGR",
-                href: "https://github.com/seqcode/pegr",
+                label: 'PEGR',
+                href: 'https://github.com/seqcode/pegr',
               },
               {
-                label: "STENCIL",
-                href: "https://github.com/CEGRcode/stencil",
+                label: 'STENCIL',
+                href: 'http://pughlab.mbg.cornell.edu/stencil',
               },
               {
-                label: "GenoPipe",
-                href: "https://github.com/CEGRcode/GenoPipe-docs",
+                label: 'GenoPipe',
+                href: 'http://pughlab.mbg.cornell.edu/GenoPipe-docs',
               },
             ],
           },
